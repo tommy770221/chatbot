7c49e5c2ed619c8d40a6db4ceb2dec09df6684d0
Tommy770221 <tommy770221@gmail.com>
2017-04-14 00:24:50 +0800

    1 spring mvc

--- src/main/java/com/tommy/chatfuel/controller/ChatFuelController.java
+++ src/main/java/com/tommy/chatfuel/controller/ChatFuelController.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 public class ChatFuelController {
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public String create(@RequestParam(value = "test") String test, HttpServletRequest httpServletRequest) {
+    public String blockTemplate(@RequestParam(value = "test") String test, HttpServletRequest httpServletRequest) {
         return "{\n" +
                 "  \"messages\": [\n" +
                 "    {\n" +
@@ -33,8 +33,61 @@ public class ChatFuelController {
                 "            },\n" +
                 "            {\n" +
                 "              \"type\": \"web_url\",\n" +
-                "              \"url\": \"https://petersapparel.parseapp.com/buy_item?item_id=100\",\n" +
+                "              \"url\": \"https://pixabay.com/zh/%E5%AE%B6%E8%9D%87-%E9%A3%9E-%E5%8A%A8%E7%89%A9-%E7%BB%BF%E8%89%B2-%E7%89%B9%E5%86%99-%E6%98%86%E8%99%AB-2221549/\",\n" +
                 "              \"title\": \"Buy Item\"\n" +
+                "            }\n" +
+                "          ]\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+    }
+
+    @RequestMapping(value = "gallery",method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public String gallery(HttpServletRequest httpServletRequest) {
+        return "{\n" +
+                " \"messages\": [\n" +
+                "    {\n" +
+                "      \"attachment\":{\n" +
+                "        \"type\":\"template\",\n" +
+                "        \"payload\":{\n" +
+                "          \"template_type\":\"generic\",\n" +
+                "          \"elements\":[\n" +
+                "            {\n" +
+                "              \"title\":\"Classic White T-Shirt\",\n" +
+                "              \"image_url\":\"https://pixabay.com/zh/%E5%AE%B6%E8%9D%87-%E9%A3%9E-%E5%8A%A8%E7%89%A9-%E7%BB%BF%E8%89%B2-%E7%89%B9%E5%86%99-%E6%98%86%E8%99%AB-2221549/\",\n" +
+                "              \"subtitle\":\"Soft white cotton t-shirt is back in style\",\n" +
+                "              \"buttons\":[\n" +
+                "                {\n" +
+                "                  \"type\":\"web_url\",\n" +
+                "                  \"url\":\"https://pixabay.com/zh/%E5%AE%B6%E8%9D%87-%E9%A3%9E-%E5%8A%A8%E7%89%A9-%E7%BB%BF%E8%89%B2-%E7%89%B9%E5%86%99-%E6%98%86%E8%99%AB-2221549/\",\n" +
+                "                  \"title\":\"View Item\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                  \"type\":\"web_url\",\n" +
+                "                  \"url\":\"https://pixabay.com/zh/%E5%AE%B6%E8%9D%87-%E9%A3%9E-%E5%8A%A8%E7%89%A9-%E7%BB%BF%E8%89%B2-%E7%89%B9%E5%86%99-%E6%98%86%E8%99%AB-2221549/\",\n" +
+                "                  \"title\":\"Buy Item\"\n" +
+                "                }\n" +
+                "              ]\n" +
+                "            },\n" +
+                "            {\n" +
+                "              \"title\":\"Classic Grey T-Shirt\",\n" +
+                "              \"image_url\":\"https://pixabay.com/zh/%E5%AE%B6%E8%9D%87-%E9%A3%9E-%E5%8A%A8%E7%89%A9-%E7%94%9F%E6%B4%BB-%E6%98%86%E8%99%AB-%E7%89%B9%E5%86%99-2222158/\",\n" +
+                "              \"subtitle\":\"Soft gray cotton t-shirt is back in style\",\n" +
+                "              \"buttons\":[\n" +
+                "                {\n" +
+                "                  \"type\":\"web_url\",\n" +
+                "                  \"url\":\"https://pixabay.com/zh/%E5%AE%B6%E8%9D%87-%E9%A3%9E-%E5%8A%A8%E7%89%A9-%E7%94%9F%E6%B4%BB-%E6%98%86%E8%99%AB-%E7%89%B9%E5%86%99-2222158/\",\n" +
+                "                  \"title\":\"View Item\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                  \"type\":\"web_url\",\n" +
+                "                  \"url\":\"https://pixabay.com/zh/%E5%AE%B6%E8%9D%87-%E9%A3%9E-%E5%8A%A8%E7%89%A9-%E7%BB%BF%E8%89%B2-%E7%89%B9%E5%86%99-%E6%98%86%E8%99%AB-2221549/\",\n" +
+                "                  \"title\":\"Buy Item\"\n" +
+                "                }\n" +
+                "              ]\n" +
                 "            }\n" +
                 "          ]\n" +
                 "        }\n" +
